952a797d79b2d0b53ad8be9379d5cd0d85ee5025
dhrumil-columbia <dhrumildparekh@gmail.com>
2016-12-18 15:08:46 -0500

    Added questions and options as a dictionary in run.py

--- run.py
+++ run.py
@@ -26,7 +26,12 @@ except LookupError:
     nltk.download('averaged_perceptron_tagger')
 ##nltk.download('wordnet')
 from forms import QuestionForm
-
+qstns=dict()
+qstn["q1"]={"text":"You are seated very close to a wailing baby and her mother on a flight. Your thoughts?","options":["Annoyed and Irritated, you have a drink to try and sleep through this","You offer to help the mother in any way you can","You ask the air hostess if any other seat is available","You hear someone else complain and you disagree with them"]}
+qstn["q2"]={"text":"You have a free weekend with nothing to do, which would you prefer?","options":["Volunteer for a child literacy program","Hangout with your friends","Sleep!!","Help a friend who is moving"]}
+qstn["q3"]={"text":"You are attending a social party, which one defines you?","options":["You are in a polite,passionate argument with someone of an opposite view","You are the life of the party filled with bubbly energy","You are busy gobbling food","You are by yourself willing for this thing to end"]}
+qstn["q4"]={"text":"Which one of this is you?","options":["At a buffet full of great food, you plan and start eating","You make long term plans to complete tasks and achieve milestone","You plan before you start learning a new skill, language etc.","You like to have company wheb you are about to learn new things","None of these"]}
+qstn["q5"]={"text":"You are working on a group project, what role will you play?","options":["The one who does not work yet shows up to take credit :p","You just go along the flow- whatever!","You are the leader- kind, generous and fair","You do your part, nothing more or less"]}
 @app.route("/question", methods=['GET','POST'])
 def question():
     form = QuestionForm()
@@ -98,7 +103,7 @@ def index():
     return render_template("index.html",freqs=lt,rname=rstrnt,rnge=rnge,images=imgs,stars=stars)
 
 def query_restaurant(query):
-    client=MongoClient('mongodb://<user>:<password>@ds139567.mlab.com:39567/w6998')
+    client=MongoClient('mongodb://yelpers:<password>@ds139567.mlab.com:39567/w6998')
     db=client.w6998
     restrnt=db.restaurants
     result=dict()
